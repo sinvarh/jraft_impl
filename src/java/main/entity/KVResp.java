5c52a41fca392ea970effdea56115e002e642cf6
sinvar <779272536@qq.com>
2022-01-15 16:29:39 +0800

    客户端请求，服务间rpc通讯

---
+++ src/java/main/entity/KVResp.java
@@ -0,0 +1,15 @@
+package main.entity;
+
+import lombok.Data;
+
+/**
+ * @program: jraft
+ * @description: 客户端key_value 请求
+ * @author: hxh E-mail:hxh@100cb.cn
+ * @create: 2022-01-10 13:23
+ */
+@Data
+public class KVResp {
+    private int code;
+    private String msg;
+}
---
+++ src/java/main/entity/RpcResp.java
@@ -0,0 +1,10 @@
+package main.entity;
+
+/**
+ * @program: jraft
+ * @description: rpc 结果
+ * @author: hxh E-mail:hxh@100cb.cn
+ * @create: 2022-01-14 00:29
+ */
+public class RpcResp {
+}
