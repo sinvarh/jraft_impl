7513187c62d5b1090530a14c69ccb573832c5086
sinvar <779272536@qq.com>
2022-01-29 22:41:07 +0800

    rocksDb操作类

--- src/java/main/DefaultLogModule.java
+++ src/java/main/DefaultLogModule.java
@@ -1,5 +1,7 @@
 package main;
 
+import com.alipay.sofa.rpc.common.json.JSON;
+
 import lombok.extern.slf4j.Slf4j;
 import main.entity.LogEntry;
 import org.rocksdb.Options;
@@ -10,84 +12,89 @@ import java.io.File;
 import java.util.List;
 
 @Slf4j
+//todo 这里的写会有并发问题
 public class DefaultLogModule implements LogModule{
+    private RocksDB db;
+    private static final String lastIndexKey = "LAST_INDEX_KEY";
+    public DefaultLogModule(String port) {
+        //初始化连接
+        final Options options = new Options().setCreateIfMissing(true);
+        String dbDir = "./rocksDB/testDB/"+port;
+        File file = new File(dbDir);
+        if(!file.exists()){
+            boolean res  = file.mkdirs();
+            if(res){
+                log.info("create file success");
+            }else{
+                log.error("create file error");
+                return;
+            }
+            log.info(file.getAbsolutePath());
+        }
+        try {
+            db = RocksDB.open(options, dbDir);
+        } catch (RocksDBException e) {
+            log.error("init db error",e);
+        }
+    }
+
     @Override
     public void appendEntries(List<LogEntry> entries) {
-
+        for(LogEntry entry: entries){
+            write(entry);
+        }
     }
 
     @Override
     public LogEntry getLast() {
+        try {
+            byte[] res = db.get(lastIndexKey.getBytes());
+            if(res==null){
+                return null;
+            }else{
+              return read(Utils.bytesToLong(res));
+            }
+        } catch (Exception e) {
+            log.error("get last",e);
+        }
         return null;
     }
 
     @Override
     public long getLastIndex() {
-        return 0;
+        LogEntry logEntry = getLast();
+        if(logEntry==null) {
+            return 0;
+        }else{
+            return logEntry.getIndex();
+        }
     }
 
     @Override
     public LogEntry read(Long index) {
+        try {
+            byte[]  res = db.get(Utils.longToBytes(index));
+            return JSON.parseObject(new String(res),LogEntry.class);
+        } catch (RocksDBException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     @Override
     public boolean removeFromStartIndex(Long startIndex) {
+        //todo 怎么删除
         return false;
     }
 
     @Override
     public void write(LogEntry logEntry) {
-
-    }
-
-    public static void main(String[] args) {
-        // a static method that loads the RocksDB C++ library.
-        RocksDB.loadLibrary();
-
-        // the Options class contains a set of configurable DB options
-        // that determines the behaviour of the database.
-        try (final Options options = new Options().setCreateIfMissing(true)) {
-
-            // a factory method that returns a RocksDB instance
-            String dbDir = "./rocksDB/testDB";
-            File file = new File(dbDir);
-            if(!file.exists()){
-                boolean res  = file.mkdirs();
-                if(res){
-                    log.info("creat file success");
-                }else{
-                    log.error("creat file error");
-                    return;
-                }
-                log.info(file.getAbsolutePath());
-            }
-
-            try (final RocksDB db = RocksDB.open(options, dbDir)) {
-
-                // do something
-
-                byte[] key1;
-                byte[] key2;
-                // some initialization for key1 and key2
-                key1 = "testKey1".getBytes();
-                key2 = "testKey2".getBytes();
-                try {
-                    final byte[] value = db.get(key1);
-                    if (value != null) {  // value == null if key1 does not exist in db.
-                        log.info(new String(value));
-                    }else {
-                        db.put(key1, "value1".getBytes());
-                        log.info("can not get value");
-                    }
-//                    db.delete(key1);
-                } catch (RocksDBException e) {
-                    // error handling
-                    log.info("error",e);
-                }
-            }
+        byte[] key =Utils.longToBytes(logEntry.getIndex());
+        byte[] lastIndexKeyByte = lastIndexKey.getBytes();
+        try {
+            db.put(key, JSON.toJSONString(logEntry).getBytes());
+            db.put(lastIndexKeyByte,Utils.longToBytes(logEntry.getIndex()));
         } catch (RocksDBException e) {
-            // do some error handling
             log.error("error",e);
         }
     }
