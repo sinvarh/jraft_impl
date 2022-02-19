521b1ff17d2adab8d78c004ca51cae3350ef2331
sinvar <779272536@qq.com>
2022-02-19 23:52:17 +0800

    增加测试用例，待完善

--- src/test/SofaRpcTest.java
+++ src/test/SofaRpcTest.java
@@ -1,20 +1,24 @@
 import com.alipay.remoting.exception.RemotingException;
-import com.alipay.remoting.rpc.RpcClient;
-import com.alipay.sofa.rpc.config.ProviderConfig;
-import com.alipay.sofa.rpc.config.ServerConfig;
 import lombok.extern.slf4j.Slf4j;
 import main.*;
+import main.config.Metadata;
+import main.config.RaftThreadPoolExecutor;
 import main.constant.CommandType;
+import main.entity.Peer;
 import main.model.log.LogEntry;
 import main.model.rpc.AppendEntriesReqs;
+import main.model.rpc.AppendEntriesResp;
 import main.model.rpc.common.RaftRpcReq;
+import main.model.rpc.common.RaftRpcResp;
 import main.rpc.RaftRpcClient;
 import main.rpc.RaftRpcServer;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.*;
+
+import static main.config.NodeStatus.LEADER;
 
 @Slf4j
 public class SofaRpcTest {
@@ -27,7 +31,7 @@ public class SofaRpcTest {
 
     @Before
     public void setUp() {
-        serverPort = 9001;
+        serverPort = 9081;
         logModule = new DefaultLogModule(serverPort);
         stateMachine = new StateMachineImpl(serverPort);
         consensus = new ConsensusImpl(logModule, stateMachine);
@@ -39,14 +43,66 @@ public class SofaRpcTest {
     @Test
     public void testAppendLog() throws RemotingException, InterruptedException {
         String addr = "127.0.0.1:" + serverPort;
-        List<LogEntry> list = Collections.singletonList(new LogEntry(1L, 1, new LogEntry.Command("1111", "2222")));
-        AppendEntriesReqs reqs = new AppendEntriesReqs(1, "127.0.0.1:9001", 0, 1, list, 1);
+        List<LogEntry> list = Collections.singletonList(new LogEntry(2L, 1, new LogEntry.Command("1111", "2222")));
+        AppendEntriesReqs reqs = new AppendEntriesReqs(1, "127.0.0.1:9001", 1, 1, list, 2);
 
-        RaftRpcReq req = new RaftRpcReq(CommandType.appendLog.getType(), reqs);
-        RaftRpcClient.getClient().invokeSync(addr, req, 300000);
-
+    //        RaftRpcReq req = new RaftRpcReq(CommandType.appendLog.getType(), reqs);
+    //        RaftRpcResp appendEntriesResp = (RaftRpcResp) RaftRpcClient.getClient().invokeSync(addr, req, 300000);
+        AppendEntriesResp resp = consensus.appendEntries(reqs);
         RaftRpcClient.getClient().shutdown();
     }
+
+    @Test
+    public void initTask(){
+
+        RaftThreadPoolExecutor raftThreadPoolExecutor;
+        //todo 优化参数
+        raftThreadPoolExecutor = new RaftThreadPoolExecutor(10, 10, 600,
+                TimeUnit.SECONDS, new ArrayBlockingQueue<>(1000), new ThreadPoolExecutor.AbortPolicy());
+
+        ScheduledThreadPoolExecutor heartbeatExecutor = new ScheduledThreadPoolExecutor(1);
+        ScheduledThreadPoolExecutor timeOutExecutor = new ScheduledThreadPoolExecutor(1);
+        Set<main.entity.Peer> peerSet = new HashSet<>();
+        peerSet.add(new Peer("127.0.0.1:9001"));
+//        peerSet.add(new Peer("127.0.0.1:9002"));
+//        peerSet.add(new Peer("127.0.0.1:9003"));
+
+
+    }
+
+
+
+    @Test
+    public void heartBeatResult(){
+            Peer p = new Peer("127.0.0.1:9001");
+            AppendEntriesReqs appendEntriesReqs = new AppendEntriesReqs();
+            Metadata.currentTerm = 10002;
+            appendEntriesReqs.setTerm(Metadata.currentTerm);
+            appendEntriesReqs.setLeaderId(null);
+
+            appendEntriesReqs.setLeaderCommit(Metadata.commitIndex);
+
+            RaftRpcReq req = new RaftRpcReq(CommandType.appendLog.getType(), appendEntriesReqs);
+            try {
+                AppendEntriesResp appendEntriesResp = (AppendEntriesResp) RaftRpcClient.getClient().invokeSync(p.getAddr(), req, 3000);
+                if(appendEntriesResp.getSuccess()){
+
+//                    reentrantLock.lock();
+//                    long currentTime = System.currentTimeMillis();
+//                    lastHeartBeatTime = Math.max(currentTime,lastHeartBeatTime);
+//                    reentrantLock.unlock();
+
+
+                }
+            } catch (Exception e) {
+                log.error("心跳远程调用error", e);
+            }
+
+
+        };
+
+//        RaftRpcReq(type=2, data=AppendEntriesReqs(term=1000000, leaderId=null, prevLogIndex=0, prevLogTerm=0, entries=null, leaderCommit=0))
+
 
 
 }
--- src/test/TestExecutor.java
+++ src/test/TestExecutor.java
@@ -1,51 +1,82 @@
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import main.config.Metadata;
+import main.config.RaftThreadPoolExecutor;
+import main.constant.CommandType;
+import main.model.rpc.AppendEntriesReqs;
+import main.model.rpc.AppendEntriesResp;
+import main.model.rpc.common.RaftRpcReq;
+import main.rpc.RaftRpcClient;
 
-import java.util.Date;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.*;
+import java.util.concurrent.*;
 
+import static main.config.NodeStatus.LEADER;
+
+@Slf4j
 public class TestExecutor {
 
     private static ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(1);
 
     static volatile long current;
     public static void main(String[] args) {
-        ScheduledExecutorService executor = Executors.newScheduledThreadPool(2);
+        RaftThreadPoolExecutor raftThreadPoolExecutor;
+        //todo 优化参数
+        raftThreadPoolExecutor = new RaftThreadPoolExecutor(10, 10, 600,
+                TimeUnit.SECONDS, new ArrayBlockingQueue<>(1000), new ThreadPoolExecutor.AbortPolicy());
 
-        current = System.currentTimeMillis();
-        executor.scheduleAtFixedRate(
-                new Runnable() {
-                    @SneakyThrows
-                    @Override
-                    public void run() {
-                        Thread.sleep(1000);
-                        System.out.println("aaaa");
-                        System.out.println(System.currentTimeMillis() - current);
-                        current = System.currentTimeMillis();
+        ScheduledThreadPoolExecutor heartbeatExecutor = new ScheduledThreadPoolExecutor(1);
+        Set<main.entity.Peer> peerSet = new HashSet<>();
+        peerSet.add(new main.entity.Peer("127.0.0.1:9001"));
+        peerSet.add(new main.entity.Peer("127.0.0.1:9002"));
+        peerSet.add(new main.entity.Peer("127.0.0.1:9003"));
+
+
+        heartbeatExecutor.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("moehrt");
+
+                List<Callable<Boolean>> callableList = new ArrayList<>(peerSet.size());
+                for (main.entity.Peer peer : peerSet) {
+                    callableList.add(heartBeatResult(peer));
+                }
+                try {
+                    List<Future<Boolean>> resList = raftThreadPoolExecutor.invokeAll(callableList);
+                    for(Future<Boolean> heartBeatRes :resList){
+                        if(heartBeatRes.get()){
+                            //todo 重试
+                            log.error("error");
+                        };
                     }
-                },
-                5,
-                5,
-                TimeUnit.SECONDS);
+                } catch (Exception e) {
+                    log.error("heart invoke all error");
+                }
+            }
+        },0,1,TimeUnit.SECONDS);
 
-        executor.scheduleAtFixedRate(
-                new Runnable() {
-                    @SneakyThrows
-                    @Override
-                    public void run() {
-                        Thread.sleep(1000);
-                        System.out.println("bbbb");
-                        System.out.println(System.currentTimeMillis() - current);
-                        current = System.currentTimeMillis();
-                    }
-                },
-                5,
-                5,
-                TimeUnit.SECONDS);
+    }
+    private static Callable<Boolean> heartBeatResult(main.entity.Peer p){
+        return () -> {
+            AppendEntriesReqs appendEntriesReqs = new AppendEntriesReqs();
+            appendEntriesReqs.setTerm(Metadata.currentTerm);
+            appendEntriesReqs.setLeaderId(Metadata.leaderAddr);
+            appendEntriesReqs.setLeaderCommit(Metadata.commitIndex);
+
+            RaftRpcReq req = new RaftRpcReq(CommandType.appendLog.getType(), appendEntriesReqs);
+            try {
+                AppendEntriesResp appendEntriesResp = (AppendEntriesResp) RaftRpcClient.getClient().invokeSync(p.getAddr(), req, 3000);
+                if(appendEntriesResp.getSuccess()){
 
 
+                    return true;
+                }
+            } catch (Exception e) {
+                log.error("心跳远程调用error", e);
+            }
+
+            return false;
+        };
     }
 
 
